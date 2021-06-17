6d9d36e008c3ecfcce3d36527768b0aa581d97d3
Quentin Chouleur <qchouleur@pobox.com>
2021-06-17 14:21:19 +0200

    Add test skeleton for interpreter tests.

---
+++ test/interpreter.test.ts
@@ -0,0 +1,53 @@
+import { Lawn } from '../src/lawn';
+import { Interpreter } from '../src/interpreter';
+
+test('Parsing lawn info with less than two paramerters should throw', () => {
+});
+
+test('Parsing lawn info with less than two paramerters should throw', () => {
+});
+
+test('Parsing lawn info with invalid width should throw', () => {
+});
+
+test('Parsing lawn info with invalid height should throw', () => {
+});
+
+test('Parsing lawn info with valid parameters should return lawn instance', () => {
+});
+
+test('Parsing mower info with less than three parameters should throw', () => {
+});
+
+test('Parsing mower info with more than three parameters should throw', () => {
+});
+
+test('Parsing mower with invalid x position should throw', () => {
+});
+
+test('Parsing mower with invalid y position should throw', () => {
+});
+
+test('Parsing mower with invalid direcition should throw', () => {
+});
+
+test('Parsing mower info with valid parameters should return mower instance', () => {
+});
+
+test('Parsing F mower instruction should return Forward instruction', () => {
+});
+
+test('Parsing R mower instruction should return Rotate right instruction', () => {
+});
+
+test('Parsing L mower instruction should return Rotate left instruction', () => {
+});
+
+test('Parsing instruction with unknown instruction letter should throw', () => {
+});
+
+test('Parsing multiple instructions should return instructions', () => {
+});
+
+test('Processing test instruction should return expected mower positions', () => {
+});
