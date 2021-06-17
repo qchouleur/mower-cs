cd43d32e284be7b03d6a2368490c7f4abd677a8c
Quentin Chouleur <qchouleur@pobox.com>
2021-06-17 16:00:09 +0200

    Add simple CLI, make process more robust.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('start');
+import * as fs from 'fs';
+import yargs from 'yargs';
+import { Interpreter } from './interpreter';
+
+const args = yargs.options({
+  file: { type: 'string', demandOption: true, alias: 'f' },
+}).argv;
+
+try {
+  console.log(args.f);
+  const source = fs.readFileSync(args.f as string, 'utf-8');
+  console.log(new Interpreter().process(source));
+} catch (err) {
+  console.error(err);
+}
